150fdf44ff9ac256c0c5d41d71135dbe8450b33f
tu4mo <tu4mo@users.noreply.github.com>
2016-11-07 20:50:39 +0200

    Reorganize server

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+mongoose.Promise = global.Promise
 mongoose.connect(process.env.MONGODB_URI)
 
 const Message = require('./models/Message')
